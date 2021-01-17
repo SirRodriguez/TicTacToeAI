23e7d4a5777f783b59397f4fbe68210273dbe66b
Christianrodriguez795@yahoo.com <christianrodriguez795@yahoo.com>
2021-01-17 15:03:37 -0800

    Started Memory AI

--- src/Game/Player/Memory/Memory.java
+++ src/Game/Player/Memory/Memory.java
@@ -1,5 +1,7 @@
 package Game.Player.Memory;
 
+import java.util.ArrayList;
+
 import Board.Board;
 import Board.Mark.Mark;
 import Game.Player.Player;
@@ -7,12 +9,33 @@ import Game.Player.Player;
 public class Memory implements Player{
     Mark type;
 
+    // Memory of each board state
+    // An array of board states
+    // Oarganized as a list of lists that are indexed by number of moves
+    // So: boardStateMemories.get(0) means all board states with no moves
+    ArrayList<ArrayList<Mark[][]>> boardStateMemories;
+
     public Memory(Mark mark){
         type = mark;
+
+        // Load memory
+        
     }
 
+    // There will be a code for the spots in the board
+    // And their likley hood of winning, losing, etc.
+    // Winning: 0
+    // Losing:  1
+    // Tie:     2
+    // Unknown: 3
+    // X:       4
+    // O:       5
     public void makeTurn(Board board) throws Exception{
-        // TODO
+        // Copy Board History
+        // copyBoardHistory(board);
+
+        // Get board state and check if it is in memory
+        
     }
 
     public void giveWin(){
